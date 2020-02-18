0ddd10fcf47dbccdcd89e83ed14b89434d2b2f10
arita37 <noelkev0@gmail.com>
2020-02-18 13:50:32 +0900

    test commit

--- mlmodels/ztest.py
+++ mlmodels/ztest.py
@@ -18,15 +18,49 @@ import torch as torch
 
 
 
-
 def main():
   print("os.getcwd", os.getcwd())
-  print(np, np.__version__)
+  print(np, np.__version__) 
   print(tf, tf.__version__)
 
 
 
 
+test_list =[
+  "python model_gluon/gluon_automl.py",
+  "python model_gluon/gluon_deepar.py",
+  "python model_gluon/gluon_ffn.py",
+
+  ###
+  "python model_keras/01_deepctr.py",
+
+
+  ###
+  "python model_keras/01_deepctr.py",
+
+
+  ###
+  "python model_tf/1_lstm.py",
+
+
+  ###
+  "python model_tch/nbeats.py",
+
+
+
+]
+
+
+for cmd in test_list :
+    print(cmd)
+    os.system( cmd )
+
+
+
 if __name__ == "__main__":
     main()
+
+
+
+
 
