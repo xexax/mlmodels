577666d3c010a88f240deae63138adcb1b9f7a52
arita37 <noelkev0@gmail.com>
2020-03-01 15:55:32 +0900

    pplm

--- mlmodels/model_tch/pplm/pplm_train.py
+++ mlmodels/model_tch/pplm/pplm_train.py
@@ -480,35 +480,12 @@ def train_discriminator(
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train a discriminator on top of GPT-2 representations")
-    parser.add_argument(
-        "--dataset",
-        type=str,
-        default="SST",
-        choices=("SST", "clickbait", "toxic", "generic"),
-        help="dataset to train the discriminator on."
-        "In case of generic, the dataset is expected"
-        "to be a TSBV file with structure: class \\t text",
-    )
-    parser.add_argument(
-        "--dataset_fp",
-        type=str,
-        default="",
-        help="File path of the dataset to use. " "Needed only in case of generic datadset",
-    )
-    parser.add_argument(
-        "--pretrained_model", type=str, default="gpt2-medium", help="Pretrained model to use as encoder"
-    )
+    parser.add_argument("--dataset", type=str, default="SST", choices=("SST", "clickbait", "toxic", "generic"), help="dataset to train the discriminator on.""In case of generic, the dataset is expected""to be a TSBV file with structure: class \\t text", )
+    parser.add_argument("--dataset_fp", type=str, default="", help="File path of the dataset to use. " "Needed only in case of generic datadset", )
+    parser.add_argument("--pretrained_model", type=str, default="gpt2-medium", help="Pretrained model to use as encoder")
     parser.add_argument("--epochs", type=int, default=10, metavar="N", help="Number of training epochs")
-    parser.add_argument(
-        "--batch_size", type=int, default=64, metavar="N", help="input batch size for training (default: 64)"
-    )
-    parser.add_argument(
-        "--log_interval",
-        type=int,
-        default=10,
-        metavar="N",
-        help="how many batches to wait before logging training status",
-    )
+    parser.add_argument("--batch_size", type=int, default=64, metavar="N", help="input batch size for training (default: 64)")
+    parser.add_argument("--log_interval", type=int, default=10, metavar="N", help="how many batches to wait before logging training status", )
     parser.add_argument("--save_model", action="store_true", help="whether to save the model")
     parser.add_argument("--cached", action="store_true", help="whether to cache the input representations")
     parser.add_argument("--no_cuda", action="store_true", help="use to turn off cuda")
