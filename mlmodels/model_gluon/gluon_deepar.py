caf06e25a4f1eb0ee3a186540afcb8ccd1f934cf
arita37 <noelkev0@gmail.com>
2020-03-26 09:43:37 +0900

    README_index_doc.txt

--- mlmodels/model_gluon/gluon_deepar.py
+++ mlmodels/model_gluon/gluon_deepar.py
@@ -27,9 +27,10 @@ import json
 
 
 
-from mlmodels.model_gluon.util import (_config_process, log,
-                                       plot_predict, plot_prob_forecasts,
-                                       save)
+
+from mlmodels.model_gluon.util import (
+    _config_process, fit, get_dataset, load, metrics,
+    plot_predict, plot_prob_forecasts, predict, save)
 
 
 from mlmodels.util import os_package_root_path, path_norm
@@ -76,118 +77,8 @@ class Model(object):
                                          trainer=trainer)
 
 
-# Model fit
-def fit(model, sess=None, data_pars=None, model_pars=None, compute_pars=None, out_pars=None, session=None, **kwargs):
-    ##loading dataset
-    """
-      Classe Model --> model,   model.model contains thte sub-model
-
-    """
-    model_gluon = model.model
-    gluont_ds = get_dataset(data_pars)
-    model.model = model_gluon.train(gluont_ds)
-    return model
 
 
-# Model predict
-def predict(model, sess=None, data_pars=None, compute_pars=None, out_pars=None, **kwargs):
-    ##  Model is class
-    ## load test dataset
-    data_pars['train'] = False
-    test_ds = get_dataset(data_pars)
-
-    ## predict
-    forecast_it, ts_it = make_evaluation_predictions(
-        dataset=test_ds,  # test dataset
-        predictor=model.model,  # predictor
-        num_samples=compute_pars['num_samples'],  # number of sample paths we want for evaluation
-    )
-
-    ##convert generator to list
-    forecasts, tss = list(forecast_it), list(ts_it)
-    forecast_entry, ts_entry = forecasts[0], tss[0]
-
-    ### output stats for forecast entry
-    if VERBOSE:
-        print(f"Number of sample paths: {forecast_entry.num_samples}")
-        print(f"Dimension of samples: {forecast_entry.samples.shape}")
-        print(f"Start date of the forecast window: {forecast_entry.start_date}")
-        print(f"Frequency of the time series: {forecast_entry.freq}")
-        print(f"Mean of the future window:\n {forecast_entry.mean}")
-        print(f"0.5-quantile (median) of the future window:\n {forecast_entry.quantile(0.5)}")
-
-    dd = {"forecasts": forecasts, "tss": tss}
-    return dd
-
-
-
-def metrics(ypred, data_pars, compute_pars=None, out_pars=None, **kwargs):
-    ## load test dataset
-    data_pars['train'] = False
-    test_ds = get_dataset(data_pars)
-
-    forecasts = ypred["forecasts"]
-    tss = ypred["tss"]
-
-    ## evaluate
-    evaluator = Evaluator(quantiles=out_pars['quantiles'])
-    agg_metrics, item_metrics = evaluator(iter(tss), iter(forecasts), num_series=len(test_ds))
-    metrics_dict = json.dumps(agg_metrics, indent=4)
-    return metrics_dict, item_metrics
-
-
-
-###############################################################################################################
-# save and load model helper function
-class Model_empty(object):
-    def __init__(self, model_pars=None, compute_pars=None):
-        ## Empty model for Seaialization
-        self.model = None
-
-
-def save(model, path):
-    if os.path.exists(path):
-        model.model.serialize(Path(path))
-
-
-def load(path):
-    if os.path.exists(path):
-        predictor_deserialized = Predictor.deserialize(Path(path))
-
-    model = Model_empty()
-    model.model = predictor_deserialized
-    #### Add back the model parameters...
-
-    return model
-
-
-
-
-
-
-# Dataaset
-def get_dataset(data_pars):
-    ##check whether dataset is of kind train or test
-    data_path = data_pars['train_data_path'] if data_pars['train'] else data_pars['test_data_path']
-
-    #### read from csv file
-    if data_pars.get("uri_type") == "pickle":
-        data_set = pd.read_pickle(data_path)
-    else:
-        data_set = pd.read_csv(data_path)
-
-    ### convert to gluont format
-    gluonts_ds = ListDataset([{FieldName.TARGET: data_set.iloc[i].values, FieldName.START: data_pars['start']}
-                              for i in range(data_pars['num_series'])], freq=data_pars['freq'])
-
-    if VERBOSE:
-        entry = next(iter(gluonts_ds))
-        train_series = to_pandas(entry)
-        train_series.plot()
-        save_fig = data_pars['save_fig']
-        # plt.savefig(save_fig)
-
-    return gluonts_ds
 
 
 
@@ -231,23 +122,6 @@ def get_params(choice="", data_path="dataset/timeseries/", config_mode="test", *
                     "plot_prob": True, "quantiles": [0.1, 0.5, 0.9]}
 
     return model_pars, data_pars, compute_pars, out_pars
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
