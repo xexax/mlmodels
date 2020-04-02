36845b1b1d09c6816971ca5a46a0850156b42c1f
arita37 <noelkev0@gmail.com>
2020-04-02 18:54:19 +0900

    run_jupyter.sh

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -4,16 +4,8 @@
 
 set -e
 
-wrapper=""
-if [[ "${RESTARTABLE}" == "yes" ]]; then
-  wrapper="run-one-constantly"
-fi
 
-if [[ ! -z "${JUPYTERHUB_API_TOKEN}" ]]; then
-  # launched by JupyterHub, use single-user entrypoint
-  exec /usr/local/bin/start-singleuser.sh "$@"
-elif [[ ! -z "${JUPYTER_ENABLE_LAB}" ]]; then
-  . /usr/local/bin/start.sh $wrapper jupyter lab "$@"
-else
-  . /usr/local/bin/start.sh $wrapper jupyter notebook "$@"
-fi
+
+jupyter lab --NotebookApp.password="$(echo hello | python -c 'from notebook.auth import passwd;print(passwd("123"))')"
+
+
