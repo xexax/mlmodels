08020971b964760a656b04d49445e0ea74973892
Kev. Noel <arita37@users.noreply.github.com>
2020-04-05 21:45:16 +0900

    Update dataloader.py

--- mlmodels/dataloader.py
+++ mlmodels/dataloader.py
@@ -96,7 +96,8 @@ class AbstractDataLoader:
         ".pkl": {"uri": "dataloader::pickle_load"},
     }
 
-    validation_split_function = [train_test_split, "test_size", {}]
+    val_split_function = [train_test_split, "test_size", {}]  #### Why not using train_test
+
 
     def __init__(self, input_pars, loader, preprocessor, output, **args):
         self._misc_dict = args if args is not None else {}
@@ -146,14 +147,14 @@ class AbstractDataLoader:
                     except:
                         misc_input.append(processed_data[m])
 
-        split_size_arg_dict = {self.validation_split_function[1]: self.test_size, **self.validation_split_function[2], } 
+        split_size_arg_dict = {self.val_split_function[1]: self.test_size, **self.val_split_function[2], } 
 
 
         if (self.test_size > 0 and not self.generator and len(Xinput) > 0 and len(yinput) > 0 ):
-            processed_data = self.validation_split_function[0]( *(Xinput + yinput), **split_size_arg_dict ) 
+            processed_data = self.val_split_function[0]( *(Xinput + yinput), **split_size_arg_dict ) 
 
         elif self.test_size > 0:
-            processed_data = self.validation_split_function[0](processed_data, **split_size_arg_dict )
+            processed_data = self.val_split_function[0](processed_data, **split_size_arg_dict )
 
         if isinstance(processed_data, Preprocessor.output_dict_type):
             processed_data = list(processed_data.values())
@@ -161,11 +162,13 @@ class AbstractDataLoader:
         if isinstance(processed_data, list) and len(processed_data) == 1:
             processed_data = processed_data[0]
 
-        self.intermediate_output = processed_data
+        self.out_tmp = processed_data
         self._interpret_output(output)
 
     def __getitem__(self, key):
         return self._misc_dict[key]
+
+
 
     def _interpret_input_pars(self, input_pars):
         
@@ -198,16 +201,15 @@ class AbstractDataLoader:
                     raise NonfileURLError()
                 file_type = os.path.splittext(path.split("/")[-1])[1]
 
+
         self.path = path
         self.path_type = path_type
         self.file_type = file_type
         self.test_size = input_pars.get("test_size", None)
         self.generator = input_pars.get("generator", False)
         if self.generator:
-            try:
                 self.batch_size = int(input_pars.get("batch_size", 1))
-            except:
-                raise NonIntegerBatchSizeError()
+
 
         self.X_cols = input_pars.get("X_cols", None)
         self.y_cols = input_pars.get("y_cols", None)
@@ -270,38 +272,38 @@ class AbstractDataLoader:
 
     def _interpret_output(self, output):
         max_len = output.get("out_max_len", None)
-        intermediate_output = self.intermediate_output
+        out_tmp = self.out_tmp
         if max_len is not None:
             try:
-                intermediate_output = self.intermediate_output[0:max_len]
+                out_tmp = self.out_tmp[0:max_len]
             except:
                 pass
         shape = output.get("shape", None)
         if shape is not None:
             if isinstance(shape[0], list):
-                for s, o in zip(shape, intermediate_output):
+                for s, o in zip(shape, out_tmp):
                     if hasattr(o, "shape") and tuple(s) != o.shape:
                         raise OutputShapeError(tuple(s), o.shape)
-            elif tuple(shape) != intermediate_output.shape:
-                raise OutputShapeError(tuple(shape), intermediate_output.shape)
+            elif tuple(shape) != out_tmp.shape:
+                raise OutputShapeError(tuple(shape), out_tmp.shape)
             self.output_shape = shape
         path = output.get("path", None)
 
         if isinstance(path, str):
-            if isinstance(intermediate_output, np.ndarray):
-                np.save(path, intermediate_output)
+            if isinstance(out_tmp, np.ndarray):
+                np.save(path, out_tmp)
 
-            elif isinstance(intermediate_output, pd.core.frame.DataFrame):
-                intermediate_output.to_csv(path)
+            elif isinstance(out_tmp, pd.core.frame.DataFrame):
+                out_tmp.to_csv(path)
 
-            elif isinstance(intermediate_output, list) and all([isinstance(x, np.ndarray) for x in intermediate_output] ): 
-                np.savez(path, *intermediate_output)
+            elif isinstance(out_tmp, list) and all([isinstance(x, np.ndarray) for x in out_tmp] ): 
+                np.savez(path, *out_tmp)
 
             else:
-                pickle.dump(intermediate_output, open(path, "wb"))
+                pickle.dump(out_tmp, open(path, "wb"))
 
         elif isinstance(path, list):
-            for p, f in zip(path, intermediate_output):
+            for p, f in zip(path, out_tmp):
                 if isinstance(f, np.ndarray):
                     np.save(p, self.f)
 
@@ -314,66 +316,68 @@ class AbstractDataLoader:
                     pickle.dump(f, open(p, "wb"))
 
     def get_data(self):
-        return self.intermediate_output
+        return self.out_tmp
 
 
 class PyTorchDataLoader(AbstractDataLoader):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu"  ### Not automatic, user supplied please
+
         if self.generator:
-            if isinstance(self.intermediate_output, list) or isinstance(self.intermediate_output, tuple ):
+            if isinstance(self.out_tmp, list) or isinstance(self.out_tmp, tuple ):
+                self.out_tmp = [ torchtext.data.Iterator(output, self.batch_size, device= device ) 
+                                 for output in self.out_tmp
+                               ]
 
-                device = torch.device("cuda" if torch.cuda.is_available() else "cpu"  ### Not automatic, user supplied please
-
-                self.intermediate_output = [ torchtext.data.Iterator(output, self.batch_size, device= device ),
-                    for output in self.intermediate_output
-                ]
             else:
-                if isinstance(
-                    self.intermediate_output, torchtext.data.Dataset
-                ) or isinstance(
-                    self.intermediate_output, torchtext.data.TabularDataset
-                ):
-                    self.intermediate_output = torchtext.data.Iterator(
-                        self.intermediate_outputs,
-                        self.batch_size,
-                        device=torch.device(
-                            "cuda" if torch.cuda.is_available() else "cpu"
-                        ),
-                    )
+                if isinstance(self.out_tmp, torchtext.data.Dataset ) or isinstance(self.out_tmp, torchtext.data.TabularDataset ):
+                    self.out_tmp = torchtext.data.Iterator(self.out_tmps, self.batch_size, device= device, )
+
+
+
+    #### Please transfer pre-processing code to preprocessor.py
+
 
     def _load_data(self, loader):
         # if loader is tabulardataset then assume its use. otherwise, assume that the loader's output is to be fed to dataset. change the split function.
         if "torchtext_fields" in loader.keys():
-            self.validation_split_function = [
-                lambda x, test_size, **args: x.split(test_size, **args),
-                "test_size",
-                {},
-            ]
+            self.val_split_function = [lambda x, test_size, **args: x.split(test_size, **args), "test_size", {}, ]
             fields = []
             field_vocab_args = {}
+
+
             for field_pars in loader["torchtext_fields"]:
                 # process each field
                 field_types = {"Field": Field}
-                field_name = field_pars["name"]
+                field_name  = field_pars["name"]
                 field_class = field_types[field_pars.get("type", "Field")]
-                field_args = field_pars.get("args", {})
+                field_args  = field_pars.get("args", {})
+
+
                 if "preprocessing" in field_pars.keys():
                     processor, _ = load_callable_from_dict(field_pars["preprocessing"])
                     field_args["preprocessing"] = Pipeline(processor)
+
                 if "postprocessing" in field_pars.keys():
                     processor, _ = load_callable_from_dict(field_pars["postprocessing"])
                     field_args["postprocessing"] = Pipeline(processor)
+
                 if "tokenize" in field_pars.keys():
                     tokenizer, _ = load_callable_from_dict(field_pars["tokenize"])
                     field_args["tokenize"] = tokenizer
+
                 field = field_class(**field_args)
                 fields.append((field_name, field))
                 field_vocab_args[field_name] = field_pars.get("vocab_args", {})
+
+
             loader_function = None
             if "data_loader" in loader.keys():
                 data_loader = loader["data_loader"]
                 loader_function, loader_args = load_callable_from_dict(data_loader)
+
                 if loader_function != torchtext.data.TabularDataset:
                     data = torchtext.data.Dataset(
                         super(PyTorchDataLoader, self)._load_data(loader),
@@ -387,15 +391,17 @@ class PyTorchDataLoader(AbstractDataLoader):
                     loader = loader.copy()
                     loader["data_loader"] = (torchtext.data.TabularDataset, arg_dict)
                     data = super(PyTorchDataLoader, self)._load_data(loader)
+
             else:
                 loader = loader.copy()
-                loader["data_loader"] = (
-                    torchtext.data.TabularDataset,
-                    {"fields": fields, "format": "csv", "skip_header": True},
-                )
+                loader["data_loader"] = (torchtext.data.TabularDataset, {"fields": fields, "format": "csv", "skip_header": True}, ) 
                 data = super(PyTorchDataLoader, self)._load_data(loader)
+
+ 
+            ##### NO Specialized code
             for f in fields:
                 f[1].build_vocab(data, **field_vocab_args[f[0]])
+
             return data
         else:
             return super(PyTorchDataLoader, self)._load_data(self, loader)
@@ -465,3 +471,6 @@ if __name__ == "__main__":
         "data_path": f"dataset/json_/textcnn_dataloader.json",
     }
     test_module("model_tch/textcnn_dataloader.py", param_pars)
+
+
+
