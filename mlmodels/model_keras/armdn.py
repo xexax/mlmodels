df2ad3472b2e4db7eb383824bc55d67285764844
MUmarAmanat <umaramanat66@gmail.com>
2020-04-06 01:41:13 +0500

    update predict returned values

--- mlmodels/model_keras/armdn.py
+++ mlmodels/model_keras/armdn.py
@@ -272,14 +272,13 @@ def test(data_path="dataset/", pars_choice="test0", config_mode="test"):
     # for prediction
     
     log("#### Predict   ####")
-    y_pred = predict(model=model, model_pars=model_pars, data_pars=data_pars)
+    y_pred, y_test = predict(model=model, model_pars=model_pars, data_pars=data_pars)
 
     log("### Plot ###")
     data_pars["predict"] = True
-    x_test, y_test = get_dataset(data_pars)
     metrics_params = {"plot_type": "line", "pred": y_pred, 
                       "outpath": out_pars["outpath"], 
-                      "actual": y_test.reshape(-1, 1)}
+                      "actual": y_test}
     metrics_plot(metrics_params)
 
     log("#### Save/Load   ###################################################")
