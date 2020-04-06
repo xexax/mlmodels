1f964ea86d3cd6a887787ca9d665c74aa4ef7074
MUmarAmanat <umaramanat66@gmail.com>
2020-04-06 18:36:29 +0500

    remove hardcoded dataset_uri

--- mlmodels/benchmark.py
+++ mlmodels/benchmark.py
@@ -65,7 +65,7 @@ def run_benchmark_all(bench_pars=None, args=None, config_mode="test"):
     pre_process(data_path=args.data_path, dataset_name=args.dataset_name, 
                 pred_length=bench_pars["pred_length"], item_id=args.item_id)
       
-    dataset_uri =  data_path + dataset_name # args.data_path  # "~/dataset/timeseries/HOBBIES_1_001_CA_1_validation.csv"  
+    dataset_uri =  args.data_path + "{}.csv".format(args.item_id)
     json_path = args.path_json
     output_path = args.path_out
     json_list = get_all_json_path(json_path)
