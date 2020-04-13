e4f2aaf581d4aed3a8c2da62cc9c995a9d4787a5
arita37 <noelkev0@gmail.com>
2020-04-13 21:41:29 +0900

    timeseries.py preprocess

--- mlmodels/preprocess/timeseries.py
+++ mlmodels/preprocess/timeseries.py
@@ -11,6 +11,34 @@ import pandas as pd
 
 
 
+import numpy as np
+
+class Preprocess_nbeats:
+
+    def __init__(self,backcast_length, forecast_length):
+        self.backcast_length = backcast_length
+        self.forecast_length = forecast_length
+    def compute(self,df):
+        df = df.values  # just keep np array here for simplicity.
+        norm_constant = np.max(df)
+        df = df / norm_constant
+        
+        x_train_batch, y = [], []
+        for i in range(self.backcast_length, len(df) - self.forecast_length):
+            x_train_batch.append(df[i - self.backcast_length:i])
+            y.append(df[i:i + self.forecast_length])
+    
+        x_train_batch = np.array(x_train_batch)[..., 0]
+        y = np.array(y)[..., 0]
+        self.data = x_train_batch,y
+        
+    def get_data(self):
+        return self.data
+        
+
+
+
+
 def pd_load(path) :
    return pd.read_csv(path_norm(path ))
 
