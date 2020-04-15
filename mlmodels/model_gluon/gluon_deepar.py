74c15d4e12fb06b100c788c2118db99ef098a12f
Asma Damani <asma.damani20@gmail.com>
2020-04-15 08:25:26 +0000

    added ssh-gen secret, deepar model test, deepar code format

--- mlmodels/model_gluon/gluon_deepar.py
+++ mlmodels/model_gluon/gluon_deepar.py
@@ -1,38 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Gluon
-
-"""
-"""
-Things to do for benchamrking
-
-1. In mlmodels.model_gluon.util
-    i- explicitly mentioned Features columns and Target columns, incase for 
-       multivariate time series forecasting
-
-2. Fit function should return two values
-    i. first is trained model
-    ii. second is session, though if their is no session use in 
-        your model then return session=None
-
-3. Run and test this model for choice="json" and prepare prod ready json file
-   which contain all params_info to run this model
-   JSON should have following parameters
-    i. data_pars
-    ii. model_pars
-    iii. compute_pars 
-    iv. out_pars (output param like model output path, plot save path, use some dummy
-                  value incase if their is no need of out_pars) 
-
-4. predict should return two arrays
-    i. first should be prediction (only array/list/numpy array)
-    ii. second would be actual (only array/list/numpy array)
-    iii. prediction and actual should have same length
-         so to easily calculate metrics using third party
-         code
-"""
-
-
 import os
 import pandas as pd
 from mlmodels.util import env_pip_check
@@ -52,7 +18,8 @@ from mlmodels.util import os_package_root_path, path_norm, log
 VERBOSE = False
 
 class Model(object):
-    def __init__(self, model_pars=None, data_pars=None, compute_pars=None, **kwargs):
+    def __init__(self, model_pars=None, data_pars=None, 
+                 compute_pars=None, **kwargs):
         ## Empty model for Seaialization
         if model_pars is None and compute_pars is None:
             self.model = None
@@ -62,7 +29,8 @@ class Model(object):
             self.model_pars = model_pars
 
             m = self.compute_pars
-            trainer = Trainer(batch_size=m['batch_size'], clip_gradient=m['clip_gradient'], ctx=m["ctx"],
+            trainer = Trainer(batch_size=m['batch_size'], clip_gradient=m['clip_gradient'], 
+                              ctx=m["ctx"],
                               epochs=m["epochs"],
                               learning_rate=m["learning_rate"], init=m['init'],
                               learning_rate_decay_factor=m['learning_rate_decay_factor'],
@@ -96,7 +64,6 @@ def _config_process(data_path, config_mode="test"):
 
 
 def get_params(choice="", data_path="dataset/timeseries/", config_mode="test", **kw):
-    
     if choice == "json":
         return _config_process(data_path, config_mode=config_mode)
 
@@ -105,9 +72,9 @@ def get_params(choice="", data_path="dataset/timeseries/", config_mode="test", *
         data_path  = path_norm( "dataset/timeseries" )   
         out_path   = path_norm( "ztest/model_gluon/gluon_deepar/" )   
         model_path = os.path.join(out_path , "model")
-        print('model_path: ',model_path)
-        data_pars = {"train_data_path": data_path + "/train_deepar.csv" , 
-                     "test_data_path":  data_path + "/test_deepar.csv" , 
+
+        data_pars = {"train_data_path": data_path + "/train_deepar.csv",
+                     "test_data_path":  data_path + "/test_deepar.csv", 
                      "train": True,
                      'prediction_length': 12, 'freq': '5min', 
                      "save_fig": "./series.png", "modelpath": model_path}
@@ -131,22 +98,16 @@ def get_params(choice="", data_path="dataset/timeseries/", config_mode="test", *
 
 
 def get_dataset(data_pars):
-
-    data_path = data_pars['train_data_path'] 
-
-    df = pd.read_csv(data_path, header=0, index_col=0)
-   
-    gluonts_ds = ListDataset([{"start": df.index[0],
-                            "target": df.value[:"2015-04-05 00:00:00"]}],
-                          freq="5min")
-
+    data_path = data_pars['train_data_path']
+    df = pd.read_csv(data_path, header=0, index_col=0)   
+    gluonts_ds = ListDataset([{"start": df.index[0],"target": df.value[:"2015-04-05 00:00:00"]}],
+                                freq="5min")
     if VERBOSE:
         entry = next(iter(gluonts_ds))
         train_series = to_pandas(entry)
         train_series.plot()
         save_fig = data_pars['save_fig']
         # plt.savefig(save_fig)
-
     return gluonts_ds
 
 
@@ -160,15 +121,17 @@ def fit(modeule,model, sess=None, data_pars=None, model_pars=None, compute_pars=
         predictor = model_gluon.train(gluont_ds)
         return predictor
 
+
 class Model_empty(object):
     def __init__(self, model_pars=None, compute_pars=None):
-        ## Empty model for Seaialization
+        # Empty model for Seaialization
         self.model = None
 
 
 def save(model, path):
     if os.path.exists(path):
         model.model.serialize(Path(path))
+
 
 def load(path):
     if os.path.exists(path):
