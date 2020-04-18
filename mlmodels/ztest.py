f9504a110e649a10a669f804616e1b3c5b98e05e
arita37 <noelkev0@gmail.com>
2020-04-18 21:20:26 +0900

    ztest.py  log

--- mlmodels/ztest.py
+++ mlmodels/ztest.py
@@ -24,7 +24,10 @@ from mlmodels.util import get_recursive_files2
 
 
 
-log_git_push = " cd /home/runner/work/mlmodels/mlmodels_store/   && ls &&  git add --all &&  git commit -m 'log'   && git push --all   && cd /home/runner/work/mlmodels/mlmodels/ "
+           
+           
+
+log_git_push = " git config --local user.email 'noelkev0@gmail.com' &&   git config --local user.name 'arita37'  &&  cd /home/runner/work/mlmodels/mlmodels_store/   && ls &&  git add --all &&  git commit -m 'log'   && git push --all   && cd /home/runner/work/mlmodels/mlmodels/ "
 
 
 
