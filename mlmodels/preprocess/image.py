c78f38bcf926342212cd04f03b9f89060315b9c6
arita37 <noelkev0@gmail.com>
2020-04-22 01:08:11 +0900

    image.py

--- mlmodels/preprocess/image.py
+++ mlmodels/preprocess/image.py
@@ -14,6 +14,7 @@ def torchvision_dataset_MNIST_load(path, **args):
                     ]))
     return train_dataset, valid_dataset  
 
+
 def wrap_torch_datasets(sets, args_list = None, **args):
     if not isinstance(sets,list) and not isinstance(sets,tuple):
         sets = [sets]
@@ -36,6 +37,50 @@ def torch_transform_mnist():
                         transforms.Normalize((0.1307,), (0.3081,))
     ])
     return transform
+
+
+
+
+
+
+def get_dataset_torch(data_pars):
+    import importlib
+    import torch
+
+    if  data_pars["transform"]  :
+       transform = getattr(importlib.import_module("mlmodels.preprocess.image"), data_pars.get("transform", "torch_transform_mnist" ))
+    else :
+       transform = None
+
+    
+    dataset_module =  data_pars.get('dataset_module', "torchvision.datasets")   
+    dset = getattr(importlib.import_module(dataset_module), data_pars["dataset"])
+
+    train_loader = torch.utils.data.DataLoader( dset(data_pars['data_path'], train=True, download=True, transform= transform),
+                                                batch_size=data_pars['train_batch_size'], shuffle=True)
+    
+    valid_loader = torch.utils.data.DataLoader( dset(data_pars['data_path'], train=False, download=True, transform= transform),
+                                                batch_size=data_pars['train_batch_size'], shuffle=True)
+
+    return train_loader, valid_loader  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
