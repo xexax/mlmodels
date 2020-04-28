eda0f517a2cbd2d633fe4843c99f1f6f87a634a2
Kev. Noel <arita37@users.noreply.github.com>
2020-04-28 23:57:48 +0900

    Update run_install.sh

--- run_install.sh
+++ run_install.sh
@@ -1,8 +1,8 @@
 
-pip install numpy==1.18.2  pillow==6.2.1 
+pip install numpy==1.18.2  pillow==6.2.1  
 mkdir z && cd z 
-cd z && git clone https://github.com/arita37/mlmodels.git
-cd z/mlmodels && pip install -e . -r requirements.txt 
-cd z/mlmodels && pip install -r requirements_fake.txt
+cd z && git clone https://github.com/arita37/mlmodels.git  && \
+cd z/mlmodels && pip install -e . -r requirements.txt   && \
+cd z/mlmodels && pip install -r requirements_fake.txt   && \
 
 
