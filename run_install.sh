0d705e7fe187fc41e9b87c94f8b277f8819285cd
arita37 <noelkev0@gmail.com>
2020-05-01 01:56:12 +0900

    run_install.sh

--- run_install.sh
+++ run_install.sh
@@ -1,6 +1,6 @@
 
 
-pip install numpy==1.18.2  pillow==6.2.1  && pip install  https://github.com/arita37/mlmodels/archive/dev.zip --progress-bar  off  --quiet  && wget https://raw.githubusercontent.com/arita37/mlmodels/dev/requirements_fake.txt  && pip install -r requirements_fake.txt  --progress-bar  off  --quiet
+pip install numpy==1.18.2  pillow==6.2.1  && pip install  https://github.com/arita37/mlmodels/archive/dev.zip  && wget https://raw.githubusercontent.com/arita37/mlmodels/dev/requirements_fake.txt  && pip install -r requirements_fake.txt  
 
 
 
