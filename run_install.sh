c960c283fd6e6fb66670833b4006de1b56a2b26b
arita37 <noelkev0@gmail.com>
2020-05-05 13:29:34 +0900

    test_dataloader.yml

--- run_install.sh
+++ run_install.sh
@@ -10,12 +10,12 @@ pip install numpy==1.18.2  pillow==6.2.1
 
 wget https://raw.githubusercontent.com/arita37/mlmodels/dev/requirements.txt  
 pip install -r requirements.txt   
-rm requirements.txt  
+rm requirements.txt  ;
 
 
 wget https://raw.githubusercontent.com/arita37/mlmodels/dev/requirements_fake.txt  
 pip install -r requirements_fake.txt   
-rm requirements_fake.txt  
+rm requirements_fake.txt  ;
 
 
 ### Extra for Colab
