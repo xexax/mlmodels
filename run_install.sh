10cef653de03cd578b7ae37998d4bea4fb08956c
arita37 <noelkev0@gmail.com>
2020-05-05 13:13:14 +0900

    run_install2.sh

--- run_install.sh
+++ run_install.sh
@@ -4,18 +4,40 @@
 
 
 
-
-### Deps
 pip install numpy==1.18.2  pillow==6.2.1  
 
-mkdir z  ;  cd z && git clone https://github.com/arita37/mlmodels.git  ; cd mlmodels && pip install -r requirements.txt  ; pip install -r requirements_fake.txt  
+
+
+wget https://raw.githubusercontent.com/arita37/mlmodels/dev/requirements.txt  
+pip install -r requirements.txt   
+rm requirements.txt  
+
+
+wget https://raw.githubusercontent.com/arita37/mlmodels/dev/requirements_fake.txt  
+pip install -r requirements_fake.txt   
+rm requirements_fake.txt  
 
 
 ### Extra for Colab
 pip install torchvision==0.4.0
 
 
-pip uninstall mlmodels ; pip install -e .  --no-deps 
+
+mkdir z  ;  cd z && git clone https://github.com/arita37/mlmodels.git  ;  
+
+
+pip uninstall mlmodels ; pip install -e .  --no-deps  --force ;
+
+
+
+python -c "import mlmodels;"
+
+
+
+
+
+
+
 
 
 
