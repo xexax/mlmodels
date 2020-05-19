40f9838931ad4ec02bac983dc6a04264b72b5728
arita37 <noelkev0@gmail.com>
2020-05-19 23:51:49 +0900

    dataloader.py

--- mlmodels/dataloader.py
+++ mlmodels/dataloader.py
@@ -375,7 +375,7 @@ def test_run_model():
         "dataset/json/refactor/charcnn.json",
         "dataset/json/refactor/charcnn_zhang.json",
         'dataset/json/refactor/torchhub_cnn_dataloader.json'
-        
+
     ]
 
     for x in ll :
@@ -388,7 +388,7 @@ def test_run_model():
             with open(data_path) as json_file:
                 config = json.load(json_file)
 
-            print2(config)    
+            print( json.dumps(config))    
             test_module(config['test']['model_pars']['model_uri'], param_pars)
 
          except Exception as e :
